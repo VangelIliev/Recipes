dd3daa944c4908f2651788bb726ddd9ba27ffa20
Vangel <venonomer7@abv.bg>
2021-07-14 23:35:56 +0300

    Mapping models

--- Recipies/Recipies/Mapping/AutoMapping.cs
+++ Recipies/Recipies/Mapping/AutoMapping.cs
@@ -10,10 +10,23 @@ namespace Recipies.Mapping
     {
         public AutoMapping()
         {
+            //Recipe
             CreateMap<Recipe, RecipeModel>();           
             CreateMap<RecipeModel, Recipe>();
             CreateMap<RecipeModel, RecipeViewModel>();
             CreateMap<RecipeViewModel, RecipeModel>();
+
+            CreateMap<Like, LikeModel>();
+            CreateMap<LikeModel, Like>();
+
+            CreateMap<Comment, CommentModel>();
+            CreateMap<CommentModel, Comment>();
+
+            CreateMap<Product, ProductModel>();
+            CreateMap<ProductModel, Product>();
+
+            CreateMap<Category, CategoryModel>();
+            CreateMap<CategoryModel, Category>();
         }
     }
 }
