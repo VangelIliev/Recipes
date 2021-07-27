5feca50ada1638baec67a8a619dd4c93dc6d6e06
Vangel <venonomer7@abv.bg>
2021-07-27 23:41:39 +0300

    adding logick to admin controller

--- Recipies/Recipies/Controllers/AdminController.cs
+++ Recipies/Recipies/Controllers/AdminController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Recipes.Domain.Contracts;
+using System.Threading.Tasks;
 
 namespace Recipies.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ICategoryService _categoryService;
+
+        public AdminController(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<ActionResult> AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddCategory()
+        {
+            return View();
+        }
     }
 }
