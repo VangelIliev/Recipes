1b76f87dbf6b0209f481c0fe5956791c09affa8f
Vangel <venonomer7@abv.bg>
2021-08-13 21:55:54 +0300

    adding email sender

--- Recipies/Recipies/Controllers/ContactController.cs
+++ Recipies/Recipies/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Recipies.Models.EmailModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace Recipies.Controllers
     {
         public async Task<ActionResult> Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Index(EmailInputFormModel model)
+        {
+            var name = model.Name;
             return View();
         }
     }
---
+++ Recipies/Recipies/Models/EmailModels/EmailInputFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipies.Models.EmailModels
+{
+    public class EmailInputFormModel
+    {
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Message { get; set; }
+
+        public string Answer  { get; set; }
+    }
+}
