4603d5b342dcfb8f86d40eea4f321b101443a54b
Vangel <venonomer7@abv.bg>
2021-08-18 12:50:59 +0300

    implementing email sender

--- Recipies/Recipies/Controllers/ContactController.cs
+++ Recipies/Recipies/Controllers/ContactController.cs
@@ -9,16 +9,18 @@ using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
 
 namespace Recipies.Controllers
 {
     public class ContactController : Controller
     {
-        private IEmailSender _emailSender;
-
-        public ContactController(IEmailSender emailSender)
+        private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
+        public ContactController(IEmailSender emailSender, IConfiguration configuration)
         {
             _emailSender = emailSender;
+            _configuration = configuration;
         }
 
         public async Task<ActionResult> Index()
@@ -42,9 +44,33 @@ namespace Recipies.Controllers
             {
                 return RedirectToAction("CustomError", "Errors");
             }                        
-           return View();
+           
         }
-       
+
+        public async Task<ActionResult> ContactHost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> ContactHost(EmailInputFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            try
+            {
+                var hostEmail = _configuration.GetValue<string>("UserName");
+                await _emailSender.SendEmailAsync("recipes.vangel@gmail.com", $"This is an email from {model.Email}", model.Message);
+                return RedirectToAction("All", "Recipes");
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("CustomError", "Errors");
+            }
+        }
+
+
     }
     
 }
